16b72a4b478e6a53c5e76c2b409d5c81d2b7b3a4
Yue <yue@potentia.asia>
2023-04-18 09:34:44 +0800

    improve button hover

--- frontend/src/component/Button.tsx
+++ frontend/src/component/Button.tsx
@@ -8,11 +8,11 @@ type Props = ButtonHTMLAttributes<HTMLButtonElement> & {
 const Button = ({ appearance = 'primary', className, ...props }: Props) => (
   <button
     className={classNames('rounded-[37px] px-10 py-4 text-[14px] outline-none', className, {
-      'bg-[#00c3ff] text-white active:bg-blue-500 disabled:bg-blue-100 disabled:text-opacity-40':
+      'bg-[#00c3ff] text-white hover:bg-[#0090ff] disabled:bg-blue-100 disabled:text-opacity-40':
         appearance === 'primary',
-      'bg-purple-500 text-white active:bg-purple-300 disabled:bg-purple-100 disabled:text-opacity-30':
+      'bg-[#4346E1] text-white disabled:bg-purple-100 disabled:text-opacity-30':
         appearance === 'secondary',
-      'border-[1px] border-solid border-black text-black bg-transparent hover:bg-blue-300 hover:border-blue-300 hover:text-white disabled:bg-white disabled:border-black disabled:text-black disabled:opacity-30':
+      'border-[1px] border-solid border-black text-black bg-transparent hover:bg-[#00c3ff] hover:border-[#00c3ff] hover:text-white disabled:bg-white disabled:border-black disabled:text-black disabled:opacity-30':
         appearance === 'border',
     })}
     {...props}
