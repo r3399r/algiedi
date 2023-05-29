1e0acae8d3bf52e9e92f475723e730784f0f2edc
Yue <yue@potentia.asia>
2023-05-29 17:53:08 +0800

    fix deployment script

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -24,12 +24,12 @@ npm i
 npm run pre:deploy
 aws cloudformation package --template-file aws/cloudformation/template.yaml --output-template-file packaged.yaml --s3-bucket gotron-cf-midway-ap-southeast-1
 aws cloudformation deploy --template-file packaged.yaml --stack-name $project-$env-stack --parameter-overrides TargetEnvr=$env Project=$project --no-fail-on-empty-changeset --s3-bucket gotron-cf-midway-ap-southeast-1 --capabilities CAPABILITY_NAMED_IAM
-# npm run copy
-# echo ====================================================================================
+npm run copy
+echo ====================================================================================
 
 echo deploy frontend to S3...
-# cd ../frontend
-# npm i
-# npm run pre:deploy
-# aws s3 sync ./dist s3://$project-$env --delete --cache-control no-cache
-# echo ====================================================================================
+cd ../frontend
+npm i
+npm run pre:deploy
+aws s3 sync ./dist s3://$project-$env --delete --cache-control no-cache
+echo ====================================================================================
