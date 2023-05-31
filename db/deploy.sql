2753e7076dafdc181b8ed33e84b44060664f4398
Yue <yue@potentia.asia>
2023-05-31 14:12:09 +0800

    remove sql

--- db/deploy.sql
+++ db/deploy.sql
@@ -2,7 +2,6 @@ source db/table/project.sql
 source db/table/user.sql
 source db/table/lyrics.sql
 source db/table/track.sql
-source db/table/project_user.sql
 source db/view/v_track.sql
 source db/view/v_lyrics.sql
 source db/view/v_creation.sql
