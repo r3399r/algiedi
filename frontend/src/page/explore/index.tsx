6b541c455207721bec931ce35b6819b26926d798
Yue <yue@potentia.asia>
2023-06-05 18:10:01 +0800

    explore page

--- frontend/src/page/explore/index.tsx
+++ frontend/src/page/explore/index.tsx
@@ -1,3 +1,151 @@
-const Explore = () => <div>Explore</div>;
+import PicSample1 from 'src/image/sample1.png';
+
+const Explore = () => (
+  <div className="bg-[#fafafa]">
+    <div className="font-bold ml-4">FEATURED SONGS</div>
+    <div className="w-full overflow-x-auto">
+      <div className="flex mb-6">
+        <div className="text-center w-[200px] flex-shrink-0">
+          <img src={PicSample1} />
+          <div>Song name</div>
+          <div>by artist</div>
+        </div>
+        <div className="text-center w-[200px] flex-shrink-0">
+          <img src={PicSample1} />
+          <div>Song name</div>
+          <div>by artist</div>
+        </div>
+        <div className="text-center w-[200px] flex-shrink-0">
+          <img src={PicSample1} />
+          <div>Song name</div>
+          <div>by artist</div>
+        </div>
+        <div className="text-center w-[200px] flex-shrink-0">
+          <img src={PicSample1} />
+          <div>Song name</div>
+          <div>by artist</div>
+        </div>
+        <div className="text-center w-[200px] flex-shrink-0">
+          <img src={PicSample1} />
+          <div>Song name</div>
+          <div>by artist</div>
+        </div>
+        <div className="text-center w-[200px] flex-shrink-0">
+          <img src={PicSample1} />
+          <div>Song name</div>
+          <div>by artist</div>
+        </div>
+        <div className="text-center w-[200px] flex-shrink-0">
+          <img src={PicSample1} />
+          <div>Song name</div>
+          <div>by artist</div>
+        </div>
+        <div className="text-center w-[200px] flex-shrink-0">
+          <img src={PicSample1} />
+          <div>Song name</div>
+          <div>by artist</div>
+        </div>
+        <div className="text-center w-[200px] flex-shrink-0">
+          <img src={PicSample1} />
+          <div>Song name</div>
+          <div>by artist</div>
+        </div>
+        <div className="text-center w-[200px] flex-shrink-0">
+          <img src={PicSample1} />
+          <div>Song name</div>
+          <div>by artist</div>
+        </div>
+      </div>
+    </div>
+    <div className="flex">
+      <div className="w-1/2 mt-4 mx-4">
+        <div className="font-bold">Music</div>
+        <div className="flex gap-4">
+          <div>This week</div>
+          <div>This month</div>
+          <div>Last month</div>
+        </div>
+        <div className="flex p-4 bg-white rounded-lg mt-4">
+          <img src={PicSample1} className="w-[150px]" />
+          <div className="m-4 flex flex-col justify-center">
+            <div className="text-bold">Music name</div>
+            <div className="text-gray-500">by artist</div>
+          </div>
+        </div>
+        <div className="flex p-4 bg-white rounded-lg mt-4">
+          <img src={PicSample1} className="w-[150px]" />
+          <div className="m-4 flex flex-col justify-center">
+            <div className="text-bold">Music name</div>
+            <div className="text-gray-500">by artist</div>
+          </div>
+        </div>
+        <div className="flex p-4 bg-white rounded-lg mt-4">
+          <img src={PicSample1} className="w-[150px]" />
+          <div className="m-4 flex flex-col justify-center">
+            <div className="text-bold">Music name</div>
+            <div className="text-gray-500">by artist</div>
+          </div>
+        </div>
+        <div className="flex p-4 bg-white rounded-lg mt-4">
+          <img src={PicSample1} className="w-[150px]" />
+          <div className="m-4 flex flex-col justify-center">
+            <div className="text-bold">Music name</div>
+            <div className="text-gray-500">by artist</div>
+          </div>
+        </div>
+        <div className="flex p-4 bg-white rounded-lg mt-4">
+          <img src={PicSample1} className="w-[150px]" />
+          <div className="m-4 flex flex-col justify-center">
+            <div className="text-bold">Music name</div>
+            <div className="text-gray-500">by artist</div>
+          </div>
+        </div>
+      </div>
+      <div className="w-1/2 mt-4 mx-4">
+        <div className="font-bold">Lyrics</div>
+        <div className="flex gap-4">
+          <div>This week</div>
+          <div>This month</div>
+          <div>Last month</div>
+        </div>
+        <div className="flex p-4 bg-white rounded-lg mt-4">
+          <img src={PicSample1} className="w-[150px]" />
+          <div className="m-4 flex flex-col justify-center">
+            <div className="text-bold">Lyrics name</div>
+            <div className="text-gray-500">by artist</div>
+          </div>
+        </div>
+        <div className="flex p-4 bg-white rounded-lg mt-4">
+          <img src={PicSample1} className="w-[150px]" />
+          <div className="m-4 flex flex-col justify-center">
+            <div className="text-bold">Lyrics name</div>
+            <div className="text-gray-500">by artist</div>
+          </div>
+        </div>
+        <div className="flex p-4 bg-white rounded-lg mt-4">
+          <img src={PicSample1} className="w-[150px]" />
+          <div className="m-4 flex flex-col justify-center">
+            <div className="text-bold">Lyrics name</div>
+            <div className="text-gray-500">by artist</div>
+          </div>
+        </div>
+        <div className="flex p-4 bg-white rounded-lg mt-4">
+          <img src={PicSample1} className="w-[150px]" />
+          <div className="m-4 flex flex-col justify-center">
+            <div className="text-bold">Lyrics name</div>
+            <div className="text-gray-500">by artist</div>
+          </div>
+        </div>
+        <div className="flex p-4 bg-white rounded-lg mt-4">
+          <img src={PicSample1} className="w-[150px]" />
+          <div className="m-4 flex flex-col justify-center">
+            <div className="text-bold">Lyrics name</div>
+            <div className="text-gray-500">by artist</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+);
 
 export default Explore;
