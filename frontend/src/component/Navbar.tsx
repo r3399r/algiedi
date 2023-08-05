a04d4e5fecf6b8203cffce89d3881a63df6eab74
Yue <yue@potentia.asia>
2023-08-05 09:00:51 +0900

    add underline

--- frontend/src/component/Navbar.tsx
+++ frontend/src/component/Navbar.tsx
@@ -1,6 +1,7 @@
+import classNames from 'classnames';
 import { useState } from 'react';
 import { useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { Page } from 'src/constant/Page';
 import Logo from 'src/image/logo.svg';
 import { RootState } from 'src/redux/store';
@@ -11,6 +12,7 @@ const Navbar = () => {
   const navigate = useNavigate();
   const [open, setOpen] = useState<boolean>(false);
   const { isLogin } = useSelector((rootState: RootState) => rootState.ui);
+  const pathname = useLocation().pathname;
 
   return (
     <>
@@ -18,32 +20,43 @@ const Navbar = () => {
         <img src={Logo} className="cursor-pointer pt-2" onClick={() => navigate(Page.Home)} />
         <div className="hidden items-center gap-10 sm:flex">
           <div
-            className="cursor-pointer decoration-blue hover:underline"
+            className={classNames('cursor-pointer decoration-blue hover:underline', {
+              underline: pathname === Page.Explore,
+            })}
             onClick={() => navigate(Page.Explore)}
           >
             Explore
           </div>
           <div
-            className="cursor-pointer decoration-blue hover:underline"
+            className={classNames('cursor-pointer decoration-blue hover:underline', {
+              underline: pathname === Page.AboutUs,
+            })}
             onClick={() => navigate(Page.AboutUs)}
           >
             About
           </div>
           <div
-            className="cursor-pointer decoration-blue hover:underline"
+            className={classNames('cursor-pointer decoration-blue hover:underline', {
+              underline: pathname === Page.Faq,
+            })}
             onClick={() => navigate(Page.Faq)}
           >
             FAQ
           </div>
           <div
-            className="cursor-pointer decoration-blue hover:underline"
+            className={classNames('cursor-pointer decoration-blue hover:underline', {
+              underline: pathname === Page.ContatUs,
+            })}
             onClick={() => navigate(Page.ContatUs)}
           >
             Contact Us
           </div>
           <Button onClick={() => navigate(isLogin ? Page.Profile : Page.Login)}>Create Now</Button>
         </div>
-        <div className="sm:hidden" onClick={() => setOpen(true)}>
+        <div
+          className="cursor-pointer decoration-blue hover:underline sm:hidden"
+          onClick={() => setOpen(true)}
+        >
           Menu
         </div>
       </div>
--- frontend/src/component/NavbarDrawer.tsx
+++ frontend/src/component/NavbarDrawer.tsx
@@ -1,5 +1,6 @@
+import classNames from 'classnames';
 import { useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { Page } from 'src/constant/Page';
 import { RootState } from 'src/redux/store';
 import Drawer, { DrawerProps } from './Drawer';
@@ -11,6 +12,7 @@ type NavbarDrawerProps = DrawerProps & {
 const NavbarDrawer = ({ open, onClose }: NavbarDrawerProps) => {
   const navigate = useNavigate();
   const { isLogin } = useSelector((rootState: RootState) => rootState.ui);
+  const pathname = useLocation().pathname;
 
   const goto = (path: Page) => () => {
     onClose();
@@ -19,22 +21,45 @@ const NavbarDrawer = ({ open, onClose }: NavbarDrawerProps) => {
 
   return (
     <Drawer anchor="right" open={open} onClose={onClose}>
-      <div className="ml-auto h-[30px] w-[30px]" onClick={onClose}>
+      <div className="ml-auto h-[30px] w-[30px] cursor-pointer" onClick={onClose}>
         x
       </div>
-      <div className="px-4 pb-1 pt-2" onClick={() => navigate(isLogin ? Page.Profile : Page.Login)}>
+      <div
+        className="mx-4 mt-3 cursor-pointer decoration-blue hover:underline"
+        onClick={() => navigate(isLogin ? Page.Profile : Page.Login)}
+      >
         Create Now
       </div>
-      <div className="px-4 pb-1 pt-2" onClick={goto(Page.Explore)}>
+      <div
+        className={classNames('mx-4 mt-3 cursor-pointer decoration-blue hover:underline', {
+          underline: pathname === Page.Explore,
+        })}
+        onClick={goto(Page.Explore)}
+      >
         Explore
       </div>
-      <div className="px-4 pb-1 pt-2" onClick={goto(Page.AboutUs)}>
+      <div
+        className={classNames('mx-4 mt-3 cursor-pointer decoration-blue hover:underline', {
+          underline: pathname === Page.AboutUs,
+        })}
+        onClick={goto(Page.AboutUs)}
+      >
         About
       </div>
-      <div className="px-4 pb-1 pt-2" onClick={goto(Page.Faq)}>
+      <div
+        className={classNames('mx-4 mt-3 cursor-pointer decoration-blue hover:underline', {
+          underline: pathname === Page.Faq,
+        })}
+        onClick={goto(Page.Faq)}
+      >
         FAQ
       </div>
-      <div className="px-4 pb-1 pt-2" onClick={goto(Page.ContatUs)}>
+      <div
+        className={classNames('mx-4 mt-3 cursor-pointer decoration-blue hover:underline', {
+          underline: pathname === Page.ContatUs,
+        })}
+        onClick={goto(Page.ContatUs)}
+      >
         Contact Us
       </div>
     </Drawer>
