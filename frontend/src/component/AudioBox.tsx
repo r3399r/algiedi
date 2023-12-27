715bfaf9bc09bffcbc211f8f9cad9f8bcb5c84b7
Yue <yue@potentia.asia>
2023-12-27 18:34:29 +0800

    fix audiobox

--- frontend/src/component/AudioBox.tsx
+++ frontend/src/component/AudioBox.tsx
@@ -1,16 +1,24 @@
 import CloseFullscreenIcon from '@mui/icons-material/CloseFullscreen';
 import FastForwardIcon from '@mui/icons-material/FastForward';
 import FastRewindIcon from '@mui/icons-material/FastRewind';
-import { useMemo, useState } from 'react';
+import classNames from 'classnames';
+import { useEffect, useMemo, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { setCurrent } from 'src/redux/playlistSlice';
 import { RootState } from 'src/redux/store';
 import Cover from './Cover';
 
 const AudioBox = () => {
-  const { playlist, current } = useSelector((rootState: RootState) => rootState.playlist);
+  const { playlist, current, isClicked } = useSelector(
+    (rootState: RootState) => rootState.playlist,
+  );
   const dispatch = useDispatch();
   const [isMinimize, setIsMinimize] = useState<boolean>(false);
+  const ref = useRef<HTMLAudioElement>(null);
+
+  useEffect(() => {
+    ref.current?.play();
+  }, [isClicked]);
 
   const creation = useMemo(() => {
     if (playlist === null || current === null || current < 0) return null;
@@ -29,50 +37,48 @@ const AudioBox = () => {
     dispatch(setCurrent(current - 1));
   };
 
-  if (isMinimize)
-    return (
-      <div className="fixed bottom-0 right-0 z-50 flex items-center gap-2 rounded-md border border-solid border-black bg-white">
-        <CloseFullscreenIcon
-          fontSize="small"
-          className="cursor-pointer"
-          onClick={() => setIsMinimize(false)}
-        />
+  return (
+    <div className="fixed bottom-0 right-0 z-50 flex items-center gap-2 rounded-xl border border-solid border-black bg-white">
+      {!isMinimize && (
+        <div className="flex flex-col items-center py-2 pl-2">
+          <Cover url={creation?.info.coverFileUrl ?? null} size={70} />
+          <div>{creation?.username ?? 'Author'}</div>
+        </div>
+      )}
+      <div className={classNames({ 'relative py-2 pr-2': !isMinimize })}>
+        {!isMinimize && (
+          <div className="mb-2 ml-4 mr-8 flex items-center justify-between">
+            <div>{creation?.info.name ?? 'Title'}</div>
+            <div className="flex gap-1">
+              <div
+                className="flex h-8 w-8 cursor-pointer items-center justify-center rounded-full border border-solid border-black bg-black"
+                onClick={onPrev}
+              >
+                <FastRewindIcon className="text-white" />
+              </div>
+              <div
+                className="flex h-8 w-8 cursor-pointer items-center justify-center rounded-full border border-solid border-black bg-black"
+                onClick={onNext}
+              >
+                <FastForwardIcon className="text-white" />
+              </div>
+            </div>
+          </div>
+        )}
         <audio
           src={creation?.fileUrl ?? ''}
           controls
-          autoPlay
           controlsList="nodownload"
-          className="hidden"
+          className={classNames({ hidden: isMinimize })}
+          ref={ref}
         />
-      </div>
-    );
-
-  return (
-    <div className="fixed bottom-0 right-0 z-50 flex items-center gap-2 rounded-xl border border-solid border-black bg-white">
-      <div className="flex flex-col items-center py-2 pl-2">
-        <Cover url={creation?.info.coverFileUrl ?? null} size={70} />
-        <div>{creation?.username ?? 'Author'}</div>
-      </div>
-      <div className="relative py-2 pr-2">
-        <div className="mb-2 ml-4 mr-8 flex items-center justify-between">
-          <div>{creation?.info.name ?? 'Title'}</div>
-          <div className="flex gap-1">
-            <div
-              className="flex h-8 w-8 cursor-pointer items-center justify-center rounded-full border border-solid border-black bg-black"
-              onClick={onPrev}
-            >
-              <FastRewindIcon className="text-white" />
-            </div>
-            <div
-              className="flex h-8 w-8 cursor-pointer items-center justify-center rounded-full border border-solid border-black bg-black"
-              onClick={onNext}
-            >
-              <FastForwardIcon className="text-white" />
-            </div>
-          </div>
-        </div>
-        <audio src={creation?.fileUrl ?? ''} controls autoPlay controlsList="nodownload" />
-        <div className="absolute right-1 top-0 cursor-pointer" onClick={() => setIsMinimize(true)}>
+        <div
+          className={classNames('cursor-pointer', {
+            static: isMinimize,
+            'absolute right-1 top-0': !isMinimize,
+          })}
+          onClick={() => setIsMinimize(!isMinimize)}
+        >
           <CloseFullscreenIcon fontSize="small" />
         </div>
       </div>
--- frontend/src/redux/playlistSlice.ts
+++ frontend/src/redux/playlistSlice.ts
@@ -2,11 +2,13 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { Playlist } from 'src/model/Playlist';
 
 export type PlaylistState = {
+  isClicked: boolean;
   playlist: Playlist[] | null;
   current: number | null;
 };
 
 const initialState: PlaylistState = {
+  isClicked: false,
   playlist: null,
   current: null,
 };
@@ -20,6 +22,7 @@ export const playlistSlice = createSlice({
     },
     setCurrent: (state: PlaylistState, action: PayloadAction<number>) => {
       state.current = action.payload;
+      state.isClicked = !state.isClicked;
     },
     removePlaylistId: (state: PlaylistState, action: PayloadAction<string>) => {
       state.playlist = state.playlist?.filter((v) => v.id !== action.payload) ?? null;
