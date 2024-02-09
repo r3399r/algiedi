eae14befb286d2de3bc46b9f10a2b445a3f65365
Yue Chang <lamplighter.planet@gmail.com>
2024-02-09 11:39:45 +0800

    add message received snackbar

--- frontend/src/service/AppService.ts
+++ frontend/src/service/AppService.ts
@@ -2,6 +2,7 @@ import { DetailedNotification } from 'src/model/backend/api/Notification';
 import { Chat, WebsocketMessage } from 'src/model/backend/api/Ws';
 import { setLastNotification } from 'src/redux/apiSlice';
 import { dispatch, getState } from 'src/redux/store';
+import { openSuccessSnackbar } from 'src/redux/uiSlice';
 import { setLastChat } from 'src/redux/wsSlice';
 import { wsStart } from 'src/util/wsTick';
 
@@ -13,8 +14,13 @@ export const wsInit = () => {
     (data) => {
       const res: WebsocketMessage = JSON.parse(data);
       if (res.a === 'channel') return;
-      if (res.a === 'chat') dispatch(setLastChat((res as WebsocketMessage<Chat>).d));
-      else dispatch(setLastNotification((res as WebsocketMessage<DetailedNotification>).d));
+      if (res.a === 'chat') {
+        dispatch(setLastChat((res as WebsocketMessage<Chat>).d));
+        if (res.d.user.id !== userId)
+          dispatch(
+            openSuccessSnackbar(`You've just received a message from ${res.d.user.username}`),
+          );
+      } else dispatch(setLastNotification((res as WebsocketMessage<DetailedNotification>).d));
     },
     () => console.log('ws error'),
     () => console.log('ws open'),
